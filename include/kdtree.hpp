4aa976570ee34ff95a0befdf409faaade50ab519
Josie Thompson <dot.slash.josie@gmail.com>
2021-06-19 16:12:34 -0700

    added more specific docs, added basic test coverage for nearest_to

--- include/kdtree.hpp
+++ include/kdtree.hpp
@@ -20,17 +20,35 @@ public:
     }
 
     /**
-     * Find the nearest k points to p
+     * Find the nearest k points to p.
+     *
+     * :param p: the point to compare to
+     * :param k: the maximum number of points to return
+     *
+     * :throws: std::invalid_argument if k is negative
+     *
+     * If the tree has less than k points, then as many points as are in the
+     * tree will be returned.
+     *
+     * The returned points will be sorted in order of closest to p.
      */
     std::vector<point> nearest_to(point const & p, int k)
     {
+        return std::vector<point>();
     }
 
     /**
      * Find the nearest k points to p within a given radius
+     *
+     * :param p: the point to compare to
+     * :param k: the maximum number of points to return
+     * :param r: the radius 
+     *
+     * :throws: std::invalid_argument if k is negative or if r is not positive
      */
     std::vector<point> nearest_within(point const & p, int k, double r)
     {
+        return std::vector<point>();
     }
 };
 }
---
+++ test/test_nearest_to.cpp
@@ -0,0 +1,143 @@
+#include <catch2/catch.hpp>
+#include "kdtree.hpp"
+#include <vector>
+#include <cstdlib>
+
+TEST_CASE("empty tree with k = 0", "[kdtree][vector]")
+{
+    using point = std::vector<int>;
+    std::vector<point> points;
+    spats::kdtree<point> index(points.begin(), points.end());
+
+    point const p{0, 0};
+    auto found = index.nearest_to(p, 0);
+
+    REQUIRE(found.empty());
+}
+
+TEST_CASE("empty tree with k = 1", "[kdtree][vector]")
+{
+    using point = std::vector<float>;
+    std::vector<point> points;
+    spats::kdtree<point> index(points.begin(), points.end());
+
+    point const p{1.0f, 2.0f};
+    auto found = index.nearest_to(p, 1);
+
+    REQUIRE(found.empty());
+}
+
+TEST_CASE("singleton tree with k = 0", "[kdtree][vector]")
+{
+    using point = std::vector<double>;
+    std::vector<point> points;
+    points.push_back(point{0, 0});
+    spats::kdtree<point> index(points.begin(), points.end());
+
+    point const p{0.0, 0.0};
+    auto found = index.nearest_to(p, 0);
+
+    REQUIRE(found.empty());
+}
+
+TEST_CASE("singleton origin tree with k = 1, p = origin", "[kdtree][vector]")
+{
+    using point = std::vector<int>;
+    std::vector<point> points;
+    points.push_back(point{0, 0});
+    spats::kdtree<point> index(points.begin(), points.end());
+
+    point const p{0, 0};
+    auto found = index.nearest_to(p, 1);
+
+    REQUIRE(found.size() == 1);
+    REQUIRE(found[0][0] == 0);
+    REQUIRE(found[0][1] == 0);
+}
+
+TEST_CASE("singleton random tree with k = 1",
+          "[kdtree][vector][abs]")
+{
+    using point = std::vector<double>;
+    std::vector<point> points;
+    points.push_back(point{3.837089043, 3.367744091});
+    spats::kdtree<point> index(points.begin(), points.end());
+
+    point const p{8.368067038, 3.221732361};
+    auto found = index.nearest_to(p, 1);
+
+    REQUIRE(found.size() == 1);
+    point const & q = found[0];
+    REQUIRE(std::abs(q[0] - 3.837089043) < 0.000000001);
+    REQUIRE(std::abs(q[0] - 3.367744091) < 0.000000001);
+}
+
+TEST_CASE("singleton random tree with k > 1",
+          "[kdtree][vector][abs]")
+{
+    using point = std::vector<float>;
+    std::vector<point> points;
+    points.push_back(point{0.28f, 2.09f});
+    spats::kdtree<point> index(points.begin(), points.end());
+
+    point const p{12.35f, 5.95f};
+    auto found = index.nearest_to(p, 3);
+
+    REQUIRE(found.size() == 1);
+
+    auto q = found[0];
+    REQUIRE(std::abs(q[0] - 0.28f) < 0.01f);
+    REQUIRE(std::abs(q[0] - 2.09f) < 0.01f);
+}
+
+TEST_CASE("random tree with k > tree size", "[kdtree][vector]")
+{
+    using point = std::vector<int>;
+    std::vector<point> points;
+    points.push_back(point{30, 43});
+    points.push_back(point{85, 31});
+    points.push_back(point{80, 43});
+    spats::kdtree<point> index(points.begin(), points.end());
+
+    point const p{40, 89};
+    auto found = index.nearest_to(p, 4);
+
+    REQUIRE(found.size() == 3);
+
+    point const & q1 = found[0];
+    REQUIRE(q1[0] == 30);
+    REQUIRE(q1[1] == 43);
+    
+    point const & q2 = found[1];
+    REQUIRE(q2[0] == 80);
+    REQUIRE(q2[1] == 43);
+
+    point const & q3 = found[2];
+    REQUIRE(q3[0] == 85);
+    REQUIRE(q3[1] == 31);
+}
+
+TEST_CASE("random tree with k < tree size", "[kdtree][vector][abs]")
+{
+    using point = std::vector<double>;
+    std::vector<point> points;
+    points.push_back(point{94.29, 77.56});
+    points.push_back(point{83.24, 34.92});
+    points.push_back(point{31.72, 71.77});
+    points.push_back(point{13.79, 51.14});
+    points.push_back(point{31.66, 72.02});
+    spats::kdtree<point> index(points.begin(), points.end());
+
+    point const p{49.88, 98.03};
+    auto found = index.nearest_to(p, 2);
+
+    REQUIRE(found.size() == 2);
+    
+    auto const & q1 = found[0];
+    REQUIRE(std::abs(q1[0] - 31.66) < 0.01);
+    REQUIRE(std::abs(q1[1] - 72.02) < 0.01);
+
+    auto const & q2 = found[1];
+    REQUIRE(std::abs(q2[0] - 31.72) < 0.01);
+    REQUIRE(std::abs(q2[1] - 71.77) < 0.01);
+}
