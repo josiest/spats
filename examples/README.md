3ca98f5cb60bde524da362783a84d1ad830a1fca
Josie Thompson <dot.slash.josie@gmail.com>
2022-02-19 20:31:17 -0800

    started adding examples

---
+++ examples/README.md
@@ -0,0 +1,3 @@
+## Build instructions
+- For the sake of seeing a practical cmake file, these examples should be
+  built independent of the spatula library
---
+++ examples/add_example.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <spatula/geometry.hpp>
+namespace sp = spatula;
+
+template<sp::vector2 Vector>
+Vector add2(Vector const & v)
+{
+    return v + Vector2(2, 2);
+}
+
+struct point { int x; int y; };
+
+namespace std {
+ostream & operator<<(ostream & os, point const & p)
+{
+    os << "{" << p.x << ", " << p.y << "}";
+    return os;
+}
+}
+
+int main()
+{
+    point const a{1, 2};
+    point const b = add2(a);
+    std::cout << "a: " << a << std::endl;
+    std::cout << "b: " << b << std::endl;
+}
