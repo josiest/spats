ccb081646925457a3a3847c7657d44a2e3357df6
Josie Thompson <dot.slash.josie@gmail.com>
2022-02-27 18:29:29 -0800

    switched to using atomic constraints for many previously defined concepts

--- include/spatula/geometry/directions.hpp
+++ include/spatula/geometry/directions.hpp
@@ -10,30 +10,50 @@
 #include <array>
 
 /** The cardinal directions. */
-namespace sp::cardinal { enum direction_name { north, east, south, west, size }; }
+namespace sp::cardinal { enum direction_name { north, east, south, west }; }
 
 namespace sp {
 
-/** An enum with sequential values defined from [0, Enum::size). */
+/** The amount of values defined by an enum type. */
 template<class Enum>
-concept ranged_enum = std::is_enum_v<Enum> and requires { Enum::size; };
+    requires std::is_enum_v<Enum>
+struct enum_size;
+
+template<class Enum>
+    requires std::is_enum_v<Enum>
+using enum_size_v = enum_size<Enum>::value;
+
+template<>
+struct enum_size<cardinal::direction_name> { using value = 4; };
+
+/** An enum with sequential values defined from [0, enum_size_v<Enum>). */
+template<class Enum>
+concept ranged_enum = std::is_enum_v<Enum> and
+                      std::convertible_to<enum_size_v<Enum>, std::size_t>;
 
 /** Convert a ranged_enum to a unit-vector. */
-template<numeric Vector, ranged_enum Enum>
-struct direction_as {
+template<ranged_enum Enum, class Vector>
+struct enum_to_vector {
     Vector operator()(Enum direction) const;
 };
+template<class Vector, ranged_enum Enum>
+Vector direction_as(Enum direction)
+{
+    return enum_to_vector<Enum, Vector>{}(direction);
+}
 
-template<numeric2d Vector>
-struct direction_as<Vector, cardinal::direction_name>{
+template<field_2d_constructible Vector>
+    requires std::constructible_from<scalar_field_t<Vector>, int>
+
+struct enum_to_vector<cardinal::direction_name, Vector>{
     Vector operator()(cardinal::direction_name dir) const
     {
-        static std::array<Vector, cardinal::direction_name::size> directions{
+        std::uint32_t const N = enum_size_v<cardinal::direction_name>;
+        static std::array<Vector, N> directions{
             /* north */ Vector(0, 1), /* east */ Vector(1, 0),
             /* south */ Vector(0, -1), /* west */ Vector(-1, 0)
         };
         return directions[dir];
-
     }
 };
 }
--- include/spatula/geometry/vectors.hpp
+++ include/spatula/geometry/vectors.hpp
@@ -5,162 +5,216 @@
 #include <concepts>
 #include <ranges>
 
-// data structures and algorithms
-#include <algorithm>
-#include <utility>
+// data types and algorithms
 #include <cstddef>
-#include <array>
+#include <utility>
+#include <algorithm>
 
 namespace sp {
+
+/** An atomic constraint for field operations. */
+template<class Field>
+constexpr bool has_field_closure =
+    std::is_same_v<decltype(std::declval<Field>() +
+                            std::declVal<Field>()), Field> and
+
+    std::is_same_v<decltype(std::declval<Field>() -
+                            std::declVal<Field>()), Field> and
+
+    std::is_same_v<decltype(std::declval<Field>() *
+                            std::declVal<Field>()), Field> and
+
+    std::is_same_v<decltype(std::declval<Field>() /
+                            std::declVal<Field>()), Field>;
+};
+
+/** A type that models the field axioms.
+ * 
+ * Semantic Requirements:
+ * 
+ * Adapted from https://mathworld.wolfram.com/FieldAxioms.html:
+ * 
+ * name             | addition          | multiplication
+ * ----------------------------------------------------------------
+ * associativity    | (a+b)+c=a+(b+c)   | (a*b)*c=a*(b*c)
+ * commutativity    | a+b=b+a           | a*b=b*a
+ * distributivity   | a(b+c)=a*b+a*c    | (a+b)*c=a*c+b*c
+ * identity         | a+0=a=0+a         | a*1=a=1*a
+ * inverses         | a-a=0             | a/a=1 if a!=0
+*/
+template<class Field>
+concept field = std::regular<Field> and has_field_closure<Field>;
 
 namespace ranges = std::ranges;
 
 //
-// Components
+// Atomic component constraints
 //
 
-/** A data type with a numeric x field. */
+/** Require a type to have specific components subset to a field. */
 template<class Vector>
-concept with_x_component = requires(Vector v) {
-    v.x and std::is_arithmetic_v<std::remove_reference_t<decltype(v.x)>>;
-};
-
-/** A data type with a numeric y field. */
+constexpr bool has_x_component = has_field_closure<
+    std::remove_reference_t<decltype(std::declval<Vector>().x)>
+>;
 template<class Vector>
-concept with_y_component = requires(Vector v) {
-    v.y and std::is_arithmetic_v<std::remove_reference_t<decltype(v.y)>>;
-};
-
-/** A data type with a numeric z field. */
+constexpr bool has_y_component = has_field_closure<
+    std::remove_reference_t<decltype(std::declval<Vector>().y)>
+>;
 template<class Vector>
-concept with_z_component = requires(Vector v) {
-    v.z and std::is_arithmetic_v<std::remove_reference_t<decltype(v.z)>>;
-};
-
-/** A data type with arbitrary numeric fields. */
+constexpr bool has_z_component = has_field_closure<
+    std::remove_reference_t<decltype(std::declval<Vector>().z)>
+>;
 template<class Vector>
-concept with_i_component = requires(Vector v, std::size_t i) {
-    v[i] and std::is_arithmetic_v<std::remove_reference_t<decltype(v[i])>>;
-};
+constexpr bool has_i_component = has_field_closure<std::remove_reference_t<
+    decltype(std::declval<Vector>()[std::declval<std::size_t>()])
+>>;
 
 //
-// Field type
+// Field type-deduction
 //
 
-/** Get the underlying field type of a vector type. */
-template<class Vector> struct field;
+/** The scalar field type of a vector. */
+template<class Vector> struct scalar_field;
 
-template<with_x_component Vector>
-struct field<Vector> {
+template<class Vector>
+    requires has_x_component<Vector>
+struct scalar_field<Vector> {
     using type = std::remove_reference_t<decltype(std::declval<Vector>().x)>;
 };
 // it's possible that a vector type may support operator[] and have an x field
 // so in order to avoid ambiguity, we must specify for operator[] and not x field
 template<class Vector>
-    requires with_i_component<Vector> and (not with_x_component<Vector>)
-struct field<Vector> {
+    requires has_i_component<Vector> and (not has_x_component<Vector>)
+struct scalar_field<Vector> {
     using type = std::remove_reference_t<
         decltype(std::declval<Vector>()[std::declval<std::size_t>()])
     >;
 };
 
 template<class Vector>
-requires with_x_component<Vector> or with_i_component<Vector>
-using field_t = typename field<Vector>::type;
+using scalar_field_t = typename scalar_field<Vector>::type;
 
 //
-// Numeric Types
+// Atomic numeric-type constraints
 //
 
-/** A data type with two numeric dimensions.
- *
- * Requirements
- *  - constructible from two numeric values
- *  - has x and y components of the same type
- */
-template<class Vector>
-concept numeric2d =
-    with_x_component<Vector> and with_y_component<Vector> and
-requires(Vector v) {
-    std::same_as<decltype(v.x), decltype(v.y)>;
-    Vector(v.x, v.y);
-};
-
-/** A data type with three numeric dimensions.
- *
- * Requirements
- *  - constructible from two numeric values
- *  - has x, y, and z components of the same type
- */
-template<class Vector>
-concept numeric3d =
-    with_x_component<Vector> and with_y_component<Vector> and
-    with_z_component<Vector> and
-requires(Vector v) {
-    std::same_as<decltype(v.x), decltype(v.y)>;
-    std::same_as<decltype(v.x), decltype(v.z)>;
-    Vector(v.x, v.y, v.z);
-};
-
-/** A general numeric type.
+/** A numeric data type.
  * 
- * Requirements
- *  - numeric2d or numeric3d or numericNd
+ * Syntactic Requirements:
+ *   A numeric data type is an atomic constraint that requires a type to have a
+ *   set of uniformly-typed components with field closure.
  */
 template<class Vector>
-concept numeric = numeric2d<Vector> or numeric3d<Vector>;
+constexpr bool is_2d_numeric =
+    has_x_component<Vector> and has_y_component<Vector> and
+    std::is_same_v<scalar_field_t<Vector>, decltype(std::declval<Vector>().x)> and
+    std::is_same_v<scalar_field_t<Vector>, decltype(std::declval<Vector>().y)>;
+
+template<class Vector>
+constexpr bool is_3d_numeric =
+    has_x_component<Vector> and has_y_component<Vector> and
+    has_z_component<Vector> and
+
+    std::is_same_v<scalar_field_t<Vector>, decltype(std::declval<Vector>().x)> and
+    std::is_same_v<scalar_field_t<Vector>, decltype(std::declval<Vector>().y)> and
+    std::is_same_v<scalar_field_t<Vector>, decltype(std::declval<Vector>().z)>;
+};
+ 
+template<class Vector>
+constexpr bool is_numeric = is_2d_numeric<Vector> or is_3d_numeric<Vector>;
 
 //
-// Vector types
+// Constructible concepts
+//
+
+/** A type that's constructible from a sequence of field values.
+ * 
+ * Semantic Requirements:
+ * 
+ * - A field_constructible type models a data type with public uniformly-typed
+ *   members that can be directly initialized through a constructor.
+ * 
+ * - The type's members have a semantically defined order, which corresponds to
+ *   the order of the constructor's parameters.
+ * 
+ * Example:
+ *     struct point { int x, y; };
+ *     static_assert<field_2d_constructible<point>>;
+ * 
+ *   The point class models field_2d_constructible because all of its members
+ *   (x and y) have the same type, and the default constructor assigns them in
+ *   the standard order of x before y.
+ * 
+ *   Note, a type such as `struct yfirst_point { int y, x; };` still models
+ *   field_2d_constructible if the context it's used in orders y before x.
+ *   However, the original point type would not model field_2d_constructible for
+ *   this context because the programmer expects point(1, 2) to assign 1 to the
+ *   y member and 2 to the x member.
+ */
+template<class Vector>
+concept field_2d_constructible = std::constructible_from<
+    Vector, scalar_field_t<Vector>, scalar_field_t<Vector>
+>;
+
+template<class Vector>
+concept field_3d_constructible = std::constrictible_from<
+    Vector, scalar_field_t<Vector>, scalar_field_t<Vector>,
+            scalar_field_t<Vector>
+>;
+
+template<class Vector>
+concept field_constructible = field_2d_constructible<Vector> or
+                              field_3d_constructible<Vector>;
+
+//
+// Vector concepts
 //
 
 /** A basic "plain old" vector.
- *
- * Requirements
- * - semiregular and numeric
  * 
  * Useful for working with c-types that haven't defined vector operations.
  */
 template<class Vector>
-concept semi_vector = std::semiregular<Vector> and numeric<Vector>;
+concept semivector2 = std::semiregular<Vector> and
+                      field_2d_constructible<Vector> and is_2d_numeric<Vector>;
 template<class Vector>
-concept semi_vector2 = std::semiregular<Vector> and numeric2d<Vector>;
+concept semivector3 = std::semiregular<Vector> and
+                      field_3d_constructible<Vector> and is_3d_numeric<Vector>;
 template<class Vector>
-concept semi_vector3 = std::semiregular<Vector> and numeric3d<Vector>;
+concept semivector = semivector2<Vector> or semivector3<Vector>;
 
-/** A type that satisfies vector closure requirements
- * 
- * Requirements
- * - closed under addition, subtraction and multiplication
- */
+/** An atomic contraint for vector operations. */
 template<class Vector>
-concept vector_closure =
-    (with_x_component<Vector> or with_i_component<Vector>) and
-requires(Vector v, field_t<Vector> c) {
-    { v + v } -> std::same_as<Vector>;
-    { v - v } -> std::same_as<Vector>;
-    { c * v } -> std::same_as<Vector>;
-};
+constexpr bool has_vector_closure =
+    std::is_same_v<decltype(std::declval<Vector>() +
+                            std::declval<Vector>()), Vector> and
+
+    std::is_same_v<decltype(std::declval<Vector>() -
+                            std::declval<Vector>()), Vector> and
+
+    std::is_same_v<decltype(std::declval<scalar_field_t<Vector>>() *
+                            std::declval<Vector>()), Vector>;
 
 /** A complete vector type.
  * 
- * Requirements
- * - regular and numeric
- * - closed under addition, subtraction and multiplication
+ * Semantic Requirements:
+ * 
+ * for two n-dimensional vectors a and b of the form
+ *  v = (v[1], v[2], ..., v[n])
+ * and an element c of their scalar field
+ * 
+ * - a + b = (a[1] + b[1], a[2] + b[2], ..., a[n] + b[n])
+ * - a - b = (a[1] - b[1], a[2] - b[2], ..., a[n] - b[n])
+ * - c * a = (c * a[1], c * a[2], ..., c * a[3])
  */
 template<class Vector>
-concept vector = std::regular<Vector> and numeric<Vector> and
-                 vector_closure<Vector>;
-
+concept vector2 = std::regular<Vector> and field_2d_constructible<Vector> and
+                  is_2d_numeric<Vector> and has_vector_closure<Vector>;
 template<class Vector>
-concept vector2 = std::regular<Vector> and numeric2d<Vector> and
-                  vector_closure<Vector>;
-
+concept vector3 = std::regular<Vector> and field_3d_constructible<Vector> and
+                  is_3d_numeric<Vector> and has_vector_closure<Vector>;
 template<class Vector>
-concept vector3 = std::regular<Vector> and numeric3d<Vector> and
-                  vector_closure<Vector>;
-
-// TODO: move to separate file
+concept vector = vector2<Vector> or vector3<Vector>;
 
 //
 // Math Utilities
@@ -170,24 +224,32 @@ concept vector3 = std::regular<Vector> and numeric3d<Vector> and
 // Ordering
 //
 
-/** Order vectors by their fields. */
-template<with_x_component Vector>
+/** Order vectors by their components. */
+template<class Vector>
+    requires has_x_component<Vector> and
+             std::totally_ordered<scalar_field_t<Vector>>
 bool least_x(Vector const & u, Vector const & v)
 {
     return u.x < v.x;
 }
-template<with_y_component Vector>
+template<class Vector>
+    requires has_y_component<Vector> and
+             std::totally_ordered<scalar_field_t<Vector>>
 bool least_y(Vector const & u, Vector const & v)
 {
     return u.y < v.y;
 }
-template<with_z_component Vector>
+template<class Vector>
+    requires has_z_component<Vector> and
+             std::totally_ordered<scalar_field_t<Vector>>
 bool least_z(Vector const & u, Vector const & v)
 {
     return u.z < v.z;
 }
-template<std::size_t i, with_i_component Vector>
-bool least_by_field(Vector const & u, Vector const & v)
+template<std::size_t i, class Vector>
+    requires has_i_component<Vector> and
+             std::totally_ordered<scalar_field_t<Vector>>
+bool least_by_component(Vector const & u, Vector const & v)
 {
     return u[i] < v[i];
 }
@@ -202,7 +264,8 @@ bool least_by_field(Vector const & u, Vector const & v)
  *   points - the input points to find the boundary of
  */
 template<ranges::input_range Range>
-    requires semi_vector2<ranges::range_value_t<Range>>
+    requires semivector2<ranges::range_value_t<Range>> and
+             std::totally_ordered<scalar_field_t<ranges::range_value_t>>
 
 auto bounding_corners2d(Range && points)
 {
